7bc0f01f60a35b4251f7c31363dc584e73ed8ccc
Developer <123030444+websitenow@users.noreply.github.com>
2024-09-24 19:07:05 -0300

    Update scrapper_functions.js

--- selenium_scrapping_js/scrapper_functions.js
+++ selenium_scrapping_js/scrapper_functions.js
@@ -1,15 +1,26 @@
 function getBase64(image) {
-  return fetch(image.src)
-    .then(response => response.blob())
-    .then(blob => {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsDataURL(blob);
-      });
-    });
-};
+    const url = image.src;
+    if (url.startsWith('http') || url.startsWith('/')) {
+        return fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar a imagem');
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                return new Promise((resolve, reject) => {
+                    const reader = new FileReader();
+                    reader.onloadend = () => resolve(reader.result);
+                    reader.onerror = reject;
+                    reader.readAsDataURL(blob);
+                });
+            });
+    } else if (url.startsWith('data:')) {
+        return Promise.resolve(url);
+    }
+    return Promise.resolve(null);
+}
 
 function getImage() {
   const img = document.querySelector("img:not([data-seleniumImageIsGetted])");
@@ -27,3 +38,5 @@ async function base64Image() {
     }
     return null;
 };
+
+//USE CODE await base64Image()
